0c3f006c16bed73e8e1208c381cc9cf30ae04794
Maeve <mariya.kuryakulova@gmail.com>
2023-02-26 16:23:55 +0000

    add-timeblock-colors
    Created and called a function that should colour-code each time block. Inside this function declares the variable that contains a value of the current hour and turns the type of this value to a number

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+var saveButton = $(".saveBtn");
+var notification = $(".notification");
+var timeBlock = $(".time-block");
+var textarea = $("textarea");
+
+
 //Display the current day at the top of the calendar
 var today = moment().format('dddd, MMMM Do');
 $("#currentDay").text(today);
+//This function will change the color of the time blocks based on the time of day
+function colorChange() {
+    //Get the current hour and turn it into a number
+    var currentTime = moment().format('H');
+    const hourNumber = parseInt(currentTime);
+}
+colorChange();
